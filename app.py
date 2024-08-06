0be51d3ba4dfb3ca18c66ad80e3adae2af3d5b89
Pushkar Singh <84075300+PushkarOP@users.noreply.github.com>
2024-08-06 09:00:47 +0530

    Update app.py

--- app.py
+++ app.py
@@ -149,4 +149,4 @@ def result(task_id):
     return jsonify({'status': task['status'], 'result': task['result']})
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0")
+    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
